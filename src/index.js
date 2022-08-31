d0794be9cc2264c19c7e7b8aeffc77d82e2900b7
Anthony shadowitz <anthonyshadowitz@Anthonys-MacBook-Pro.local>
2022-08-31 14:46:44 -0400

    connecting to mongodb

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+const mongoUri =
+  "mongodb+srv://anthony:wcsVsjuz3l8oO7vD@cluster0.gk5ylbm.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose.connect(mongoUri);
+
+mongoose.connection.on("connected", () => {
+  console.log("Connected to mongo instance");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error("Error connecting to mongo", err);
+});
 
 app.get("/", (req, res) => {
   res.send("hi there");
